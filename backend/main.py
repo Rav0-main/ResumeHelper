2f32eb2556c2c4d8722e623fe741ebbc92b159ce
Rav0 <ivateem@gmail.com>
2026-05-17 18:31:24 +0300

    Benchmarks.:

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Resume Helper", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "null"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
